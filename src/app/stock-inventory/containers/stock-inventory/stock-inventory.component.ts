89d98ac271e55db675cef880780058b271d303ad
Steven Lin <steven_lx@outlook.com>
2019-10-29 21:07:17 -0700

    FormControls and FormGroups

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,14 +1,20 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'stock-inventory',
     styleUrls: ['stock-inventory.component.scss'],
-    template: `
-        <div class="stock-inventory">
-            Hello World!
-        </div>
-    `
+    templateUrl: 'stock-inventory.component.html' 
 })
 export class StockInventoryComponent {
+    form = new FormGroup({
+        store: new FormGroup({
+            branch: new FormControl('B182'),
+            code: new FormControl('1234')
+        })    
+    });
     
+    onSubmit() {
+        console.log('Submit:', this.form.value);
+    }
 }
